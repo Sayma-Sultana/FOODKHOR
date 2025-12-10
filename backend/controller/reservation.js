9eb883afcc5d337b32bf78caa9cdf6ca96d25ffb
Sayma-Sultana <saymasultanar3@gmail.com>
2025-12-11 02:06:10 +0600

    successfully database connection, done with the backend..... for now.

--- backend/controller/reservation.js
+++ backend/controller/reservation.js
@@ -1,25 +1,24 @@
-import { json } from "express";
+// import { json } from "express";
 import ErrorHandler from "../error/error.js";
 import { Reservation } from "../models/reservationSchema.js";
 
-export const sendReservation = async (req, res, next)=>{
-    const { firstName, lastName, email, phone, date, time } = req.body;
-    if( !firstName || !lastName || !email || !phone || !date || !time) {
+export const sendReservation = async (req, res, next) => {
+    const { firstName, lastName, email, date, time, phone } = req.body;
+    if( !firstName || !lastName || !email || !date || !time || !phone ) {
         return next(new ErrorHandler("Please fill full reservation form!", 400));
     }
+
     try {
-        await Reservation.create(firstName, lastName, email, phone, date, time);
-        res.status(200),
-        json({
+        await Reservation.create({firstName, lastName, email, date, time, phone});
+        res.status(201).json({
             success: true,
             message: "Reservation sent successfully!",
         });
     } catch (error) {
-        if(error.name === "Validation error"){
-            const ValidationErrors = Object.values(error.errors).map(
-                (err) => err.message
-            );
-            return next(new ErrorHandler(ValidationErrors.join(" , "),400));
+        if(error.name === 'ValidationError'){
+            const ValidationErrors = Object.values(error.errors).map(err => err.message);
+            return next(new ErrorHandler(ValidationErrors.join(' , '), 400));
         }
+        return next(error);
     }
 };
